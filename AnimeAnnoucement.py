7656e2e4c95a66ce772115e6f00d084ee212229d
brownpanda <56075784+brownpanda1@users.noreply.github.com>
2020-12-04 14:36:00 +0800

    Update AnimeAnnoucement.py

--- AnimeAnnoucement.py
+++ AnimeAnnoucement.py
@@ -21,6 +21,7 @@ def getEpisodes(anime):
     link = part.find('img')['src']
     part2 = soup.find('div', class_='infoepbox')
     episodeList = part2.find_all('a')
+    status = soup.find_all('div', class_='infodes2')[1].find_all('div', class_='textc')[-3].getText()
     episodes = []
     for line in episodeList:
         try:
@@ -28,33 +29,73 @@ def getEpisodes(anime):
             episodes.append(episode)
         except:
             pass
-    return episodes, link
+    return episodes, link, status
 
-"""Check for new anime episode"""
-def checkNewEpisode(anime):
+def checkStatus(anime):
+    url = 'https://animeheaven.ru/detail/' + anime + '-TV'
+    uClient = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    try:
+        webpage = urlopen(uClient).read()
+    except HTTPError as err:
+        if err.code == 404:
+            url = 'https://animeheaven.ru/detail/' + anime
+            uClient = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+            webpage = urlopen(uClient).read()
+
+    webpage = webpage.decode('utf-8')
+    soup = BeautifulSoup(webpage, 'html.parser')
+    status = soup.find_all('div', class_='infodes2')[1].find_all('div', class_='textc')
+    return status[-3].getText()
+
+"""Check for new anime episode based on logger file"""
+def checkNewEpisode():
     # Check if logger file exists
-    name = anime.replace(' ', '-')
-    file_name = name+'.txt'
-    file_exists = os.path.exists(file_name)
+    file_exists = os.path.exists('logger.txt')
+    new = False
+    newAnimes = []
     if file_exists:
-        with open(file_name, 'r+') as logger:
-            newEpisodes, link = getEpisodes(name)
-            newEpisodes.reverse()
-            oldEpisodes = logger.readline()
-            oldEpisodes = oldEpisodes.split(',')[:-1]
-            for i in range(len(oldEpisodes)):
-                oldEpisodes[i] = int(oldEpisodes[i])
-            if oldEpisodes[-1] != newEpisodes[-1]:
-                logger.write(str(newEpisodes[-1]) + ',')
-                logger.close()
-                return newEpisodes[-1], link
-
-    else:
-        with open(file_name, 'w') as logger:
-            newEpisodes, link = getEpisodes(name)
-            newEpisodes.reverse()
-            for episodes in newEpisodes:
-                logger.write(str(episodes) + ',')
-            logger.close()
-    return False, link
-
+        with open('logger.txt', 'r+') as file:
+            data = file.read()
+            data = data.split('\n')
+            for i in range(0,len(data)-1,2):
+                anime = data[i].replace(' ','-')
+                newEpisodes, link, status = getEpisodes(anime)
+                if checkStatus(anime) == 'Ongoing':
+                    newEpisodes = newEpisodes[::-1]
+                    if data[i+1] == '':
+                        data[i+1] = newEpisodes
+                    else:
+                        oldEpisodes = data[i+1].split(',')[:-1]
+                        if oldEpisodes[-1] != str(newEpisodes[-1]):
+                            data[i+1] = newEpisodes
+                            anime = data[i] + ',' + str(newEpisodes[-1]) + ',' + link
+                            newAnimes.append(anime)
+                            new = True
+                else:
+                    del data[i]
+                    del data[i+1]
+            rewrite = ''
+            i = 0
+            while i < len(data)-1:
+                eps = ''
+                if type(data[i]) == list:
+                    for lines in data[i]:
+                        eps += str(lines) + ','
+                    rewrite += str(eps) + '\n'
+                else:
+                    rewrite += str(data[i]) + '\n'
+                i += 1
+            if new == True:
+                final = ''
+                for episodes in data[-1]:
+                    final += str(episodes) + ','
+                file.seek(0, 0)
+                file.write(rewrite)
+                file.close()
+                return newAnimes
+            else:
+                rewrite += data[-1]
+                file.seek(0, 0)
+                file.write(rewrite)
+                file.close()
+                return False
